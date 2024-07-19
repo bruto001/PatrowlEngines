80f1754f950dd26f64f890a2285e59037dca6d79
MaKyOtOx <nicolas@patrowl.io>
2024-07-19 09:54:31 +0200

    ARS-487 - nmap - avoid PTR hostname during parsing

--- engines/nmap/__init__.py
+++ engines/nmap/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__title__ = 'patrowl_engine_nmap'
-__version__ = '1.5.0'
-__author__ = 'Nicolas MATTIOCCO'
-__license__ = 'AGPLv3'
-__copyright__ = 'Copyright (C) 2018-2022 Nicolas Mattiocco - @MaKyOtOx'
+__title__ = "patrowl_engine_nmap"
+__version__ = "1.5.1"
+__author__ = "Nicolas MATTIOCCO"
+__license__ = "AGPLv3"
+__copyright__ = "Copyright (C) 2018-2024 Nicolas Mattiocco - @MaKyOtOx"
